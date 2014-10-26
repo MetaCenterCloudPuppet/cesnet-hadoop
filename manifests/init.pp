00b5e6908eddb4cbe4d32737735123dba840b9f2
František Dvořák <valtri@civ.zcu.cz>
2014-10-26 22:50:19 +0100

    Fine tuning of Hadoop cluster daemons deployment.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: hadoop
 #
-# Setup Hadoop Cluster. Security and Kerberos are suported.
+# Setup Hadoop Cluster. Security and Kerberos are supported.
 #
 # === Parameters
 #
@@ -16,8 +16,26 @@
 # [*replication*] (1)
 #   Number of replicas.
 #
+# [*cluster_name*] ("")
+#   Name of the cluster, used during initial formatting of HDFS.
+#
 # [*realm*]
-#   Kerberos realm. Empty string disables Kerberos authentication. Required parameter.
+#   Kerberos realm. Required parameter, empty string disables Kerberos authentication.
+#
+# [*namenode_hostname*] (undef)
+#   Name Node machine. Used *hdfs_hostname* by default.
+#
+# [*resourcemanager_hostname*] (undef)
+#   Resource Manager machine. Used *yarn_hostname* by default.
+#
+# [*historyserver_hostname*] (undef)
+#   History Server machine. Used *yarn_hostname* by default.
+#
+# [*nodemanager_hostnames*] (undef)
+#   Array of Node Manager machines. Used *slaves* by default.
+#
+# [*datanodes_hostnames*] (undef)
+#   Array of Data Node machines. Used *slaves* by default.
 #
 class hadoop (
 	$hdfs_hostname = $params::hdfs_hostname,
@@ -26,24 +44,45 @@ class hadoop (
 	$cluster_name = $params::cluster_name,
 	$replication = $params::replication,
 	$realm,
+
+	$namenode_hostname = undef,
+	$resourcemanager_hostname = undef,
+	$historyserver_hostname = undef,
+	$nodemanager_hostnames = undef,
+	$datanode_hostnames = undef,
 ) inherits hadoop::params {
 	include 'stdlib'
 
-	if $yarn_hostname and !$rm_hostname { $rm_hostname = $yarn_hostname }
+	if $namenode_hostname { $nn_hostname = $namenode_hostname }
+	else { $nn_hostname = $hdfs_hostname }
+	if $resourcemanager_hostname { $rm_hostname = $resourcemanager_hostname }
+	else { $rm_hostname = $yarn_hostname }
+	if $historyserver_hostname { $hs_hostname = $historyserver_hostname }
+	else { $hs_hostname = $yarn_hostname }
+	if $nodemanager_hostnames { $nm_hostnames = $nodemanager_hostnames }
+	else { $nm_hostnames = $slaves }
+	if $datanode_hostnames { $dn_hostnames = $datanode_hostnames }
+	else { $dn_hostnames = $slaves }
 
-	if $fqdn == $hdfs_hostname {
+	if $fqdn == $nm_hostname {
 		$daemon_namenode = 1
 		$mapred_user = 1
 
 	}
 
-	if $fqdn == $yarn_hostname {
+	if $fqdn == $rm_hostname {
 		$daemon_resourcemanager = 1
+	}
+
+	if $fqdn == $hs_hostname {
 		$daemon_historyserver = 1
 	}
 
-	if member($slaves, $fqdn) {
+	if member($nm_hostnames, $fqdn) {
 		$daemon_nodemanager = 1
+	}
+
+	if member($dn_hostnames, $fqdn) {
 		$daemon_datanode = 1
 	}
 
