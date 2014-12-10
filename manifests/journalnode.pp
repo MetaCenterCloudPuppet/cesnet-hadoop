beb0cf549f18c63928bea9cbede88df45b8bd2c2
František Dvořák <valtri@civ.zcu.cz>
2014-12-10 23:36:31 +0100

    It looks like the communication between nn and jn requires plain https.

--- manifests/journalnode.pp
+++ manifests/journalnode.pp
@@ -2,10 +2,16 @@
 #
 # Main Haddop Journal Node class for Quorum Journal Manager. This is needed only when using two Name Nodes with HA. The alternative is to use NFS shared storage.
 #
+# Requires configured https on HDFS.
+#
 class hadoop::journalnode {
   include 'hadoop::journalnode::install'
   include 'hadoop::journalnode::config'
   include 'hadoop::journalnode::service'
+
+  if !$hadoop::https {
+    warn('https is required for using journal nodes')
+  }
 
   Class['hadoop::journalnode::install'] ->
   Class['hadoop::journalnode::config'] ~>
