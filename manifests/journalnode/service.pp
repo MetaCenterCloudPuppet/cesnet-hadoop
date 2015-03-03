d800add724f2628315e2b0546f3e575267d7c6c3
František Dvořák <valtri@civ.zcu.cz>
2015-03-03 14:25:50 +0100

    Journalnode doesn't require namenodes.

--- manifests/journalnode/service.pp
+++ manifests/journalnode/service.pp
@@ -8,9 +8,4 @@ class hadoop::journalnode::service {
     enable    => true,
     subscribe => [File['core-site.xml'], File['hdfs-site.xml']],
   }
-
-  if $hadoop::daemon_namenode {
-    include hadoop::namenode::service
-    Class['hadoop::namenode::service'] -> Class['hadoop::journalnode::service']
-  }
 }
