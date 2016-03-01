c2a451d06e96486ea3771cdf1350f27d62dd4fe0
František Dvořák <valtri@civ.zcu.cz>
2016-03-01 09:33:19 +0100

    Destroy kerberos ticket only when security is enabled (be symetric with the kinit resource).

--- manifests/kdestroy.pp
+++ manifests/kdestroy.pp
@@ -12,13 +12,15 @@ define hadoop::kdestroy($touchfile = $title, $touch = true) {
   $puppetfile = "/var/lib/hadoop-hdfs/.puppet-${touchfile}"
 
   if $hadoop::zookeeper_deployed {
-    exec { "kdestroy-${touchfile}":
-      command     => 'kdestroy',
-      path        => $path,
-      environment => $env,
-      onlyif      => "test -n \"${hadoop::realm}\"",
-      user        => 'hdfs',
-      creates     => $puppetfile,
+    if $hadoop::realm and $hadoop::realm != '' {
+      exec { "kdestroy-${touchfile}":
+        command     => 'kdestroy',
+        path        => $path,
+        environment => $env,
+        onlyif      => "test -n \"${hadoop::realm}\"",
+        user        => 'hdfs',
+        creates     => $puppetfile,
+      }
     }
 
     if $touch {
