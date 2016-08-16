fb22c88b207cf6d82c3dea850fdcabcd1b5418db
František Dvořák <valtri@civ.zcu.cz>
2016-08-17 01:39:46 +0200

    Small comment update for HDFS bootstrap.

--- manifests/namenode/bootstrap.pp
+++ manifests/namenode/bootstrap.pp
@@ -3,6 +3,9 @@
 # Format Hadoop Filesystem on the second HDFS Name Node. The first Name Node needs to be already formatted, empty and running.
 #
 class hadoop::namenode::bootstrap {
+  #
+  # Not creating the directory here:
+  #
   # 1) directory /var/lib/hadoop-hdfs/hdfs may be created by other
   #    daemons, format command will create the namenode subdirectory
   # 2) prefix can be different and/or there can be other replicated locations
